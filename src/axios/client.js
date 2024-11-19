67e910b1fd7887de1f2b47dc1beb8fc74500af24
Benjamin Mensah <Bmensah72538@gmail.com>
2024-11-18 19:06:52 -0600

    Modified axios instance to connect to a cloud based API.

--- src/axios/client.js
+++ src/axios/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
 const client = axios.create({
-    baseURL: 'http://127.0.0.1:8080'
+    baseURL: apiUrl
   });
 
 client.interceptors.request.use(
