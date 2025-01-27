d266559294e2ae6ca21f6932cb9bf247f7c986aa
Benjamin Mensah <bmensah72538@gmail.com>
2025-01-27 18:28:48 +0900

    Added sidebar files.

---
+++ src/components/ChatSidebar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styles from "./ChatSidebar.module.css";
+import { useChatContext } from "../context/chatContext";
+
+const Sidebar = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const { currentChatroom } = useChatContext(); 
+    const users = currentChatroom.participants;
+
+    console.log(currentChatroom);
+
+    // Filter users by search query
+    const filteredUsers = users.filter((user) =>
+        user.user.username.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    return (
+        <div className={`sidebar ${isExpanded ? "expanded" : "collapsed"}`}>
+            <button onClick={() => setIsExpanded(!isExpanded)}>
+                {isExpanded ? "Close" : "Users"}
+            </button>
+            {isExpanded && (
+                <div className="sidebar-content">
+                    <input
+                        type="text"
+                        placeholder="Search users..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <ul>
+                        {filteredUsers.map((user) => (
+                            <li key={user._id} className="user-item">
+                                <div className="user-info">
+                                    <span className="user-avatar">{/* Avatar */}</span>
+                                    <span className="user-name">{user.user.username}</span>
+                                    {/* <span className={`status ${user.isOnline ? "online" : "offline"}`}></span> */}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Sidebar;
